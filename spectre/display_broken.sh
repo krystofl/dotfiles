7e780441195f1e9b04f634b61ad4379782f43694
Krystof Litomisky <krystof@litomisky.com>
2019-09-28 21:05:50 +0800

    For the broken display, make sure it's the right way up first

--- spectre/display_broken.sh
+++ spectre/display_broken.sh
@@ -2,11 +2,13 @@
 
 # use only the working part of spectre's display
 
+# just in case, set the display orientation to be normal first
+xrandr --output eDP-1 --rotate normal
+
 # Create the modeline:
 # $ cvt 1440 1080
 # 1440x1080 59.99 Hz (CVT 1.56M3) hsync: 67.19 kHz; pclk: 129.00 MHz
 # Modeline "1440x1080_60.00"  129.00  1440 1528 1680 1920  1080 1083 1087 1120 -hsync +vsync
-
 
 # Create the mode; gonna call it BROKEN:
 xrandr --newmode BROKEN 129.00  1440 1528 1680 1920  1080 1083 1087 1120 -hsync +vsync
