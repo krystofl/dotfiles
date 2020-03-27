3dcc5dfc87593bc52a32713e793801d48d1aa346
Krystof Litomisky <krystof@litomisky.com>
2020-03-27 14:37:16 -0700

    Added script to export Atom config to files in this repo

---
+++ atom/export_atom_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# export Atom's current config to files in this folder so they can be used
+# to update the repo
+
+# settings
+cp ~/.atom/config.cson ~/.atom/keymap.cson ~/.atom/linter-config.json .
+
+# package list
+apm list --installed --bare > atom_packages.list
