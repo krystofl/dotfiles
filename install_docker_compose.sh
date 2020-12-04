df300b01c1c508821cafab1fd26985fc7aa65c35
Krystof Litomisky <krystof@litomisky.com>
2020-12-04 12:18:43 -0800

    Added script to install docker compose

---
+++ install_docker_compose.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install Docker Compose
+# Instructions from https://docs.docker.com/compose/install/
+
+# IMPORTANT: The path is hardcoded to a specific version. You may wish to update it
+
+# download
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# make it executable
+sudo chmod +x /usr/local/bin/docker-compose
