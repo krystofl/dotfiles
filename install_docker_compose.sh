7c818eee3fe10c74fa5d5f36bd57922a0e5489f0
Krystof Litomisky <krystof@litomisky.com>
2021-05-24 12:10:30 -0700

    Updated docker compose version

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -6,7 +6,7 @@
 # IMPORTANT: The path is hardcoded to a specific version. You may wish to update it
 
 # download
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # make it executable
 sudo chmod +x /usr/local/bin/docker-compose
